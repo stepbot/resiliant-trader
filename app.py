703323576cbd2bd473e6e174017fbd941dd384cc
stepbot <stephanbotes@gmail.com>
2017-09-08 12:40:29 -0400

    fixed list return

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 	installed_packages = pip.get_installed_distributions()
 	installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
 	        for i in installed_packages])
-	return installed_packages_list
+	return jsonify(installed_packages_list)
 
 if __name__ == "__main__":
 	app.run()
