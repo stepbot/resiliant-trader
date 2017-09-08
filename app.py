81493bff7b5333b8f01b23c8b134b902191cdd38
stepbot <stephanbotes@gmail.com>
2017-09-08 13:39:22 -0400

    corrected historical endpoint

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 	my_trader = Robinhood.Robinhood()
 
 	#Get a stock's quote
-	return jsonify(my_trader.get_historical_quotes("AAPL","5minute","week"))
+	return jsonify(my_trader.get_historical_quote("AAPL","5minute","week"))
 
 if __name__ == "__main__":
 	app.run()
